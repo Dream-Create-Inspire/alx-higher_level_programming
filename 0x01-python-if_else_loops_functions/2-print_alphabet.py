1d4d81cee7caf61e63dea35ad29305d8495539f9
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-21 19:39:59 +0200

    Task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-# Iterate through the alphabet using ASCII
-print(''.join([chr(i) for i in range(97, 123)]), end='')
+# Iterate through the alphabet using ASCII and print without newline
+print(''.join(f"{chr(97 + i)}" for i in range(26)), end='')
