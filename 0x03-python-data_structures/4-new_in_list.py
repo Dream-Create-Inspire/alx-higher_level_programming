5240a1b78972d5b23ace740c34c89b3c7647b1d8
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-24 20:28:16 +0200

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,13 +5,3 @@ def new_in_list(my_list, idx, element):
         return my_list[:]
     new_list = my_list[:]
     new_list[idx] = element
-    print(f"Original list: {my_list}")
-    print(f"New list: {new_list}")
-    return new_list
-
-# Example usage
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
