a3b7ce084943fef720857550dcf84f282d58d2d8
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-24 19:41:24 +0200

    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,18 @@
+def new_in_list(my_list, idx, element):
+    """
+    Replaces an element in the list at a specific position without modifying the original list.
+
+    Args:
+        my_list (list): The original list.
+        idx (int): The index where the replacement should occur.
+        element: The new element to replace the existing one.
+
+    Returns:
+        list: A new list with the modified element (if valid index), otherwise a copy of the original list.
+    """
+    if idx >= 0 and idx < len(my_list):
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
+    else:
+        return my_list
