e779460536612215d973a76a831b9f373976fa10
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-24 22:08:29 +0200

    Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 
-# Checker 4
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list[:]
     return my_list[:idx] + my_list[idx + 1:]
-
-# Checker 5
-def divisible_by_2(my_list=[]):
-    return [num % 2 == 0 for num in my_list]
-
-# Checker 6
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
-        print()
-
