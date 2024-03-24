3751612755d778a6698b415351d85798eca328bb
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-24 17:56:28 +0200

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    """Prints all integers in the list, one per line."""
     for num in my_list:
-        print("{}".format(num))
+        print("{:d}".format(num))
+
+
+# Test the function
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
