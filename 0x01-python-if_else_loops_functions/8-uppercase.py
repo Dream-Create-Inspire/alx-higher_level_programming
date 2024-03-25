68a8c3057fb2373234b63bae4d42002eeb27dae8
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-25 18:02:44 +0200

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,28 @@
  #!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
-    print()
+def uppercase(s):
+    lines = s.split('\n')  # Split the input into lines
+    for line in lines:
+        words = line.split(' ')  # Split each line into words
+        for i, word in enumerate(words):
+            for char in word:
+                ascii_value = ord(char)
+                if 97 <= ascii_value <= 122:  # Check if lowercase
+                    ascii_value -= 32  # Convert to uppercase ASCII value
+                print(chr(ascii_value), end='')
+            if i < len(words) - 1:  # Check if it's not the last word
+                print(' ', end='')  # Print space between words
+        print()  # Print newline between lines
+
+# Test cases
+test_cases = [
+    "holberton",
+    "Holberton School",
+    "Holberton School, 98 battery street",
+    "",
+    "98",
+    "z"
+]
+
+# Run the test cases
+for case in test_cases:
+    uppercase(case)
