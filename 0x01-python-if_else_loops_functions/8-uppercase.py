9e1329ee27022081b0b0874c2bfcb0cc526f20cb
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-25 16:41:06 +0200

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 def uppercase(s):
-    for char in s:
+    for i in range(len(s)):
+        char = s[i]
         ascii_value = ord(char)
         if 97 <= ascii_value <= 122:  # Check if lowercase
             ascii_value -= 32  # Convert to uppercase ASCII value
