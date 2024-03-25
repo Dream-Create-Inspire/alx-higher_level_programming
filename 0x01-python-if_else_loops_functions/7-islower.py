4e2f737e5fdf47201ddfeb11c81cdb308f5432d3
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-25 15:54:24 +0200

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,5 @@
-	#!/usr/bin/python3
-	
-	def islower(c):
-	    if isinstance(c, str) and len(c) == 1:
-	        return ord('a') <= ord(c) <= ord('z')
-	    else:
-	        return False
-	
-	# Test the function
-	print(islower('a'))  # True
-	print(islower('A'))  # False
-	print(islower('z'))  # True
-	print(islower('Z'))  # False
-print(islower('1'))  # False
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    return False
