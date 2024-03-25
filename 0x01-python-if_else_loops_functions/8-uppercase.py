44e94ac3af2e03d3469cd0b3292db82a978fc70d
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-25 16:42:34 +0200

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,7 @@
-#!/usr/bin/env python3
-def uppercase(s):
-    for i in range(len(s)):
-        char = s[i]
-        ascii_value = ord(char)
-        if 97 <= ascii_value <= 122:  # Check if lowercase
-            ascii_value -= 32  # Convert to uppercase ASCII value
-        print(chr(ascii_value), end='')
-    print()  # Print newline
-
-# Test the function
-s = "hello, world!"
-uppercase(s)
+ #!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+    print("{:s}".format(i), end="")
+    print()
