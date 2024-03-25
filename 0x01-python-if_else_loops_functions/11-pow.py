faf466ea6c7485f3cf8f45af41f48fd275aecba8
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-03-25 20:02:23 +0200

    Task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    result = 1
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result /= a
+    return result
+
+# Test the function
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
