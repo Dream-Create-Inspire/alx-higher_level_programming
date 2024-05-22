fd13919b88d4b374ab2f51467c6f2be4b9ea3b54
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-05-22 15:59:11 +0200

    Task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,2 @@
  -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT
-    COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM
-    information_schema.columns
-WHERE
-    table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SHOW CREATE TABLE first_table;
