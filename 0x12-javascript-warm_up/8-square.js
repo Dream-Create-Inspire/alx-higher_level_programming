5339cff74467ea7b092c0d10e28e7aef66a18fe6
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-19 05:52:35 +0200

    Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Get the first argument and convert it to an integer
+const size = Number.parseInt(args[0], 10);
+
+// Check if the conversion is successful
+if (Number.isNaN(size)) {
+    console.log("Missing size");
+} else {
+    // Print the square
+    for (let i = 0; i < size; i++) {
+        console.log("X".repeat(size));
+    }
+}
