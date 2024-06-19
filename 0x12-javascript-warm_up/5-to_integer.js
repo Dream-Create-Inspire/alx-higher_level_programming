0f355ccc63be134ff4f52b43ea4423f3cc7c517b
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-19 05:43:36 +0200

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Get the first argument
+const firstArg = args[0];
+
+// Convert the first argument to an integer
+const number = Number.parseInt(firstArg, 10);
+
+// Check if the conversion is successful
+const output = !Number.isNaN(number) ? `My number: ${number}` : "Not a number";
+
+// Print the output
+console.log(output);
