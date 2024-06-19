4ece407963e5b6d4d9889ee7033f4dc1646dad2c
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-19 05:27:52 +0200

    Task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Check if there are any arguments
+const firstArg = args[0];
+
+// Print the first argument or "No argument" if none are passed
+const output = firstArg ? firstArg : "No argument";
+console.log(output);
