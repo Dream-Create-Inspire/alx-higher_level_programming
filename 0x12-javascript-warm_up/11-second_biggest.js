191c1df4009146026b31fb3338133c2579fcaa8a
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-19 06:17:41 +0200

    Task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Get the arguments passed to the script
+const args = process.argv.slice(2);
+
+// Convert arguments to integers and sort them in descending order
+const numbers = args.map(Number).sort((a, b) => b - a);
+
+// Determine the second biggest integer
+let secondBiggest = 0;
+if (numbers.length > 1) {
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] < numbers[i - 1]) {
+            secondBiggest = numbers[i];
+            break;
+        }
+    }
+}
+
+// Print the second biggest integer
+console.log(secondBiggest);
