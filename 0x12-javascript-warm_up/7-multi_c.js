c1c6426ba358b40415338924e384727320951b5a
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-19 05:49:13 +0200

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+// Get the first argument and convert it to an integer
+const x = Number.parseInt(args[0], 10);
+
+// Check if the conversion is successful
+if (Number.isNaN(x)) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}
