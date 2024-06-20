9a2caebc0618dde588bcc3d13955790bc1ff0b15
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-20 17:25:14 +0200

    Task 0

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -3,4 +3,4 @@ class Rectangle {
     // The class is currently empty
 }
 
-module.exports = Rectangle; // This line is useful if you are working in a Node.js environment and want to export the class.
+module.exports = Rectangle; // Exporting the class for usage in other files
