67834f56e8b08993d198536cc1e7ff7aa9143483
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-20 18:44:33 +0200

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,14 +8,13 @@ const numbers = args.map(Number).sort((a, b) => b - a);
 // Determine the second biggest integer
 let secondBiggest = 0;
 if (numbers.length > 1) {
-for (let i = 1; i < numbers.length; i++) {
-if (numbers[i] < numbers[i - 1]) {
-secondBiggest = numbers[i];
-break;
-}
-}
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < numbers[i - 1]) {
+      secondBiggest = numbers[i];
+      break;
+    }
+  }
 }
 
 // Print the second biggest integer
-console.log(secondBiggest); //this is the expected output 
-
+console.log(secondBiggest); // this is the expected output
