cb62a65a35d8cf1a0569d24d93418023ecc2b17e
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-20 18:16:28 +0200

    Task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,9 +3,8 @@
 const args = process.argv.slice(2);
 
 // Get the first and second arguments
-const firstArg = args[0] ? args[0] : "undefined";
-const secondArg = args[1] ? args[1] : "undefined";
+const firstArg = args[0] ? args[0] : 'undefined';
+const secondArg = args[1] ? args[1] : 'undefined';
 
 // Print the output in the format " is "
 console.log(`${firstArg} is ${secondArg}`);
-
