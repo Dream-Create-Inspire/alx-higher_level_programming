1477fb08c04e659a5f301f9a52379fd518b4cf27
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-06-20 18:23:13 +0200

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Create an array with the strings to be printed
-const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 // Loop through the array and print each message
 for (const message of messages) {
-    console.log(message);
+  console.log(message);
 }
