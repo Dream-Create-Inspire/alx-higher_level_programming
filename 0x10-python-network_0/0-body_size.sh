085c13d388c16c5ef0d75daaacb6a48a7cb28c11
Dream-Create-Inspire <mmolotsinthabiseng@gmail.com>
2024-07-09 13:26:15 +0200

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a request to the URL and display the size of the response body in bytes
-curl -s "$1" -o /dev/null -w '%{size_download}\n'
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes.
+curl -s "$1" | wc -c
